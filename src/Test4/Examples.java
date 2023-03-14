9e7cb7d43a4138e3972ea5737434e53749c9bcab
Raushan4K <raushan4kmaurya@gmail.com>
2023-03-14 11:28:56 +0530

    first time commited

---
+++ src/Test4/Examples.java
@@ -0,0 +1,20 @@
+package Test4;
+
+public class Examples {
+
+	public static void main(String[] args) 
+	{
+		int x=12;
+		int y=-12;
+	    System.out.println("========For positive number=====");
+		System.out.println(x<<1);  //24
+		System.out.println(x>>1);  //6
+		System.out.println(x>>>1);  //6
+		System.out.println("======For negative number====");
+		System.out.println(y<<1);  //-24
+		System.out.println(y>>1);   //-6
+		System.out.println(y>>>1);   //2147483642
+       
+	}
+
+}
